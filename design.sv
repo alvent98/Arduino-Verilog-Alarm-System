ebf25eda12085b90e00fd315fcb697eb7b401028
alvent98 <48288106+alvent98@users.noreply.github.com>
2022-06-04 21:21:54 +0300

    Alarm off only when input 4
    Co-Authored-By: tzecris <59621325+tzecris@users.noreply.github.com>

--- design.sv
+++ design.sv
@@ -1,6 +1,6 @@
-module mux8_to_2 (m1, m2, r, k0, k1, k2, k3, k4, active, alarm);
+module alarm (m1, m2, r, k0, k1, k2, k3, k4, active, alarm);
   output active, alarm;
-  input  k0, k1, k2, k3, k4;
+  input k0, k1, k2, k3, k4;
   input m1, m2, r;
   reg active, alarm;
   always @(k0 or k1 or k2 or k3 or k4 or m1 or m2 or r)
@@ -9,7 +9,7 @@ module mux8_to_2 (m1, m2, r, k0, k1, k2, k3, k4, active, alarm);
       5'b00001: 
         begin
           active = 1; 
-          if((m1 == 1) | (m2 == 1) | (r ==1))
+          if((m1 == 1) | (m2 == 1) | (r == 1) | (alarm == 1))
             alarm = 1;
           else
             alarm = 0;
--- testbench.sv
+++ testbench.sv
@@ -1,18 +1,17 @@
 module top();
   reg m1, m2, r, k0, k1, k2, k3, k4;
   wire active, alarm;
-  mux8_to_2 my_mux(m1, m2, r, k0, k1, k2, k3, k4, active, alarm);
+  alarm my_alarm(m1, m2, r, k0, k1, k2, k3, k4, active, alarm);
   initial
   begin
-//     $dumpfile("Mux.vcd");
-//     $dumpvars(0);
     $display("\t\ttime\tm1\tm2\tr\tk0\tk1\tk2\tk3\tk4   active   alarm");
     $monitor($time ,"\t%b\t%b\t%b\t%b\t%b\t%b\t%b\t%b\t%b\t%b",m1,m2,r,k0,k1,k2, k3,k4,active,alarm);
 //     Arm is 10000, disarm is 00100
-    {m1,m2,r}=3'b001;
+    {m1,m2,r}=3'b000;
     {k4,k3,k2,k1,k0}=5'b10000;
     #10
-    {m1,m2,r}=3'b000;
+    {m1,m2,r}=3'b011;
+    #10
     {k4,k3,k2,k1,k0}=5'b00100;
     #10
     {m1,m2,r}=3'b101;
@@ -22,6 +21,10 @@ module top();
     {k4,k3,k2,k1,k0}=5'b10000;
     #10
     {m1,m2,r}=3'b011;
+    #10
+    {m1,m2,r}=3'b000;
+    #10
+    {k4,k3,k2,k1,k0}=5'b00100;
     #10 $finish;
   end
 endmodule
